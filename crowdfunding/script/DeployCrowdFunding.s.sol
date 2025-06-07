466b30b4914de6be5350d9f60e269b36630eb8da
Sol Urbano <surbano@fi.uba.ar>
2025-06-07 01:44:59 -0300

    feat: Complete implementation of NFT contract and its integration with CrowdFunding, rewardRandomFunder function to reward a random funder with an NFT

--- crowdfunding/script/DeployCrowdFunding.s.sol
+++ crowdfunding/script/DeployCrowdFunding.s.sol
@@ -5,16 +5,17 @@ pragma solidity ^0.8.18;
 import { Script } from "forge-std/Script.sol";
 import { CrowdFunding } from "../src/CrowdFunding.sol";
 import { HelperConfig } from "./HelperConfig.s.sol";
+import { NFT } from "../src/NFT.sol";
 
 contract DeployCrowdFunding is Script {
     function run() external returns (CrowdFunding crowdFunding) {
-
         // antes del broadcast no estamos enviando transacciones
         HelperConfig helperConfig = new HelperConfig();
         address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
 
+        NFT nft = new NFT();
         vm.startBroadcast(); // inicia la transmisión de transacciones
-        crowdFunding = new CrowdFunding(ethUsdPriceFeed);
+        crowdFunding = new CrowdFunding(ethUsdPriceFeed, address(nft));
         vm.stopBroadcast();
         
         return crowdFunding;
